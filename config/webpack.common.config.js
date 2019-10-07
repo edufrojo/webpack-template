2bf727d8c6e5d89b516f456d1e0c685d67fd6aec
edufrojo <edufrojo@gmail.com>
2019-10-07 23:58:01 +0200

    (feat): limpiando "dist" con plugin

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -27,7 +28,8 @@ module.exports = {
         from: "./src/assets/images",
         to: "assets/images"
       }
-    ])
+    ]),
+    new CleanWebpackPlugin()
   ],
   resolve: {
     extensions: [".js", ".ts"]
